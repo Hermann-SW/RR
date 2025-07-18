ff8af26d076fc5f8266367546a07fc2329a81d21
Hermann-SW <hermann@stamm-wilbrandt.de>
2025-07-18 12:00:19 +0200

    enhance tool name, add comment

--- data/tsp/tsplib95/opt_length.py
+++ data/tsp/tsplib95/opt_length.py
@@ -1,3 +1,8 @@
+"""
+pi@raspberrypi5:~/RR/data/tsp/tsplib95 $ ~/venv/bin/python opt_length.py ../pcb442
+../pcb442 :  [50778]
+pi@raspberrypi5:~/RR/data/tsp/tsplib95 $ 
+"""
 import sys, tsplib95
 
 name = sys.argv[1]
