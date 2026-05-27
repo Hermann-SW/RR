febcad9b4649eb6e0553db34b7a6e85d073e5510
Hermann-SW <hermann@stamm-wilbrandt.de>
2026-05-28 00:03:54 +0200

    fixed problems with m512_add openmp for loop reduction

--- tsp/openmp/mona-lisa100K.cpp
+++ tsp/openmp/mona-lisa100K.cpp
@@ -8,25 +8,28 @@ echo off | sudo tee /sys/devices/system/cpu/smt/control
 echo 0 | sudo tee /proc/sys/kernel/perf_event_paranoid
 perf stat -a -e fp_ops_retired_by_width.pack_512_uops_retired,cycles,instructions,task-clock ./$f
 
-- single omp thread faster than 8
-- sequential without avx512 slower than single omp thread only after
-  repeating the computations 500,000 times in new REPEAT(block)
-- maybe needed addition of sqrt for TSP best insert will make the difference
+- now single omp thread slower than 8
+- single thread faster than autovectorized sequential
+- warning on #pragma inside (REPEAT) macro ignored for now
 
 hermann@8840hs:~/RR/tsp/openmp$ OMP_NUM_THREADS=1 ./$f
 Starting benchmark(s) using 1 threads...
 ... completed
-Execution Time: 1.30092 seconds
+Execution Time: 1.33151 seconds
 Sum of two sqs: 190899739
-Execution Time: 3.16036 seconds
-sequential: 190899739
+Execution Time: 3.24091 seconds
+sequential    : 190899739
+Execution Time: 6.86801 seconds
+sequent. noavx: 190899739
 hermann@8840hs:~/RR/tsp/openmp$ OMP_NUM_THREADS=8 ./$f
 Starting benchmark(s) using 8 threads...
 ... completed
-Execution Time: 2.00539 seconds
+Execution Time: 0.252588 seconds
 Sum of two sqs: 190899739
-Execution Time: 3.18885 seconds
-sequential: 190899739
+Execution Time: 3.24941 seconds
+sequential    : 190899739
+Execution Time: 6.8669 seconds
+sequent. noavx: 190899739
 hermann@8840hs:~/RR/tsp/openmp$
 */
 #include <omp.h>
@@ -47,38 +50,31 @@ int32_t horizontal_add_s32(__m512i v) {
   return _mm512_reduce_add_epi32(v);
 }
 
+#pragma omp declare reduction(v512_add : __m512i : \
+    omp_out = _mm512_add_epi32(omp_out, omp_in)) \
+    initializer(omp_priv = _mm512_setzero_si512())
+
 void bench1() {
   auto start_time = std::chrono::high_resolution_clock::now();
 
-  int32_t sum[2*16] = {0};  // 16C/32T AMD 7950X
-  int total_threads = omp_get_num_threads();
-
-  #pragma omp parallel
-  {
-    int thread_id = omp_get_thread_num();
-    __m512i acc = _mm512_setzero_si512();
+  int32_t sum = 0;
+  __m512i acc = _mm512_setzero_si512();
 
 REPEAT(
-    acc = _mm512_setzero_si512();
+  acc = _mm512_setzero_si512();
 
-    for (int i = 0; i < N; i+=32) {
-      __m512i a = _mm512_load_si512((const __m512i*)(xy_even+i));
-      __m512i b = _mm512_load_si512((const __m512i*)(xy_odd+i));
+  #pragma omp parallel for reduction(v512_add:acc)
+  for (int i = 0; i < N; i+=32) {
+    __m512i a = _mm512_load_si512((const __m512i*)(xy_even+i));
+    __m512i b = _mm512_load_si512((const __m512i*)(xy_odd+i));
 
-      __m512i dxy = _mm512_sub_epi16(a, b);
+    __m512i dxy = _mm512_sub_epi16(a, b);
 
-      // Multiply pairs of s16, widen to s32, and add to accumulator
-      // Resolves 32 pairs of s16 down to 16 lanes of s32
-      acc = _mm512_dpwssd_epi32(acc, dxy, dxy);
-    }
-)
-
-    sum[thread_id] = _mm512_reduce_add_epi32(acc);
-  }  // omp parallel
-
-  for (int i = 1; i < total_threads; ++i) {
-    sum[0] += sum[i];
+    acc = _mm512_dpwssd_epi32(acc, dxy, dxy);
   }
+
+  sum = _mm512_reduce_add_epi32(acc);
+)
 
   std::cout << "... completed\n";
 
@@ -86,7 +82,30 @@ REPEAT(
     std::chrono::high_resolution_clock::now() - start_time;
 
   std::cout << "Execution Time: " << duration.count() << " seconds\n";
-  std::cout << "Sum of two sqs: " << sum[0] << "\n";
+  std::cout << "Sum of two sqs: " << sum << "\n";
+}
+
+__attribute__((target("no-avx")))
+void sequential_noavx() {
+  auto start_time = std::chrono::high_resolution_clock::now();
+  int64_t sum = 0;
+
+REPEAT(
+  sum = 0;
+  for (int i = 0; i < N; i+=32) {
+    for (int j = 0; j < 32; j+=2) {
+      const int16_t dx = xy_even[i+j] - xy_odd[i+j];
+      const int16_t dy = xy_even[i+j+1] - xy_odd[i+j+1];
+      sum += (dx*dx + dy*dy);
+    }
+  }
+)
+
+  std::chrono::duration<double> duration =
+    std::chrono::high_resolution_clock::now() - start_time;
+
+  std::cout << "Execution Time: " << duration.count() << " seconds\n";
+  std::cout << "sequent. noavx: " << sum << "\n";
 }
 
 void sequential() {
@@ -108,7 +127,7 @@ REPEAT(
     std::chrono::high_resolution_clock::now() - start_time;
 
   std::cout << "Execution Time: " << duration.count() << " seconds\n";
-  std::cout << "sequential: " << sum << "\n";
+  std::cout << "sequential    : " << sum << "\n";
 }
 
 int main() {
@@ -123,6 +142,7 @@ int main() {
   bench1();
 
   sequential();
+  sequential_noavx();
 
   return 0;
 }
